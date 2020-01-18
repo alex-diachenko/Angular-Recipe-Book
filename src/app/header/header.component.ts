f14deb91bee6c1a9c2f3839e7581a0589df1e035
alex-diachenko <dyachenko664@gmail.com>
2020-01-18 20:37:06 +0200

    added navigation and allowed to add new ingredients

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
     selector: 'app-header',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
 })
 
 export class HeaderComponent {
-    collapsed = true; //может и не нужно, надо дальше посмотреть!
+    collapsed = true; 
+   @Output() featureSelected = new EventEmitter<string>();
+
+    onSelect(feature: string) {
+        this.featureSelected.emit(feature);
+    }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output  } from '@angular/core';
 import { Recipe } from '../recipe.model';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,12 +8,16 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'first recipe for project', 'https://st3.depositphotos.com/4011313/19127/i/1600/depositphotos_191279212-stock-photo-meat-fried-grill-selective-focus.jpg')
+    new Recipe('A test recipe', 'first recipe for project', 'https://st3.depositphotos.com/4011313/19127/i/1600/depositphotos_191279212-stock-photo-meat-fried-grill-selective-focus.jpg'), 
+    new Recipe('A test recipe', 'first recipe for project', 'https://cdn.sm-news.ru/wp-content/uploads/2019/10/09/gril-1-1200x675.jpg'), 
   ];
   constructor() { }
 
   ngOnInit() {
   }
-
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
